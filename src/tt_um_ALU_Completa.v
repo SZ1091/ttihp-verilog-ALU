05d8f50f56374024855af5eb55b3d1f765638ca7
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 01:20:44 -0600

    Update tt_um_ALU_Completa.v

--- src/tt_um_ALU_Completa.v
+++ src/tt_um_ALU_Completa.v
@@ -1,19 +1,22 @@
 `timescale 1ns / 1ps
 
 module tt_um_ALU_Completa(input wire [15:0] sw,
-                    input wire btnL, btnC, btnU, btnD, btnR,
-                    output wire [12:0] led);   // Elementos reales
+                    input wire btnL, btnC, btnU, btnD, btnR, ui_7, // ui_7 es el interruptor
+                    output wire [7:0] uo); // Salidas ajustadas
 
 wire [1:0] Cantidad;
 wire [2:0] ALUControl;
+wire [7:0] resultado;
+wire Carry, Overflow, Negative, Zero;
 
-// Para Procesamiento_Entradas
+// Procesamiento de entradas
 Procesamiento_Entradas Entradas (.btnU(btnU), .btnD(btnD), .btnL(btnL), .btnR(btnR), .btnC(btnC), .ALUControl(ALUControl), .Cantidad(Cantidad));
 
-// Para ALU_S_Flags
-ALU_S_Flags ALU_F (.A(sw[7:0]), .B(sw[15:8]), .ALUControl(ALUControl), .Cantidad(Cantidad), .Resultado(led[7:0]), .Carry(led[12]), .Overflow(led[11]), .Negative(led[10]), .Zero(led[9]));
+// ALU con flags
+ALU_S_Flags ALU_F (.A(sw[7:0]), .B(sw[15:8]), .ALUControl(ALUControl), .Cantidad(Cantidad), .Resultado(resultado), .Carry(Carry), .Overflow(Overflow), .Negative(Negative), .Zero(Zero));
 
-// Para Procesamiento_Salidas
-// Procesamiento_Salidas Salidas(.Resultado(Resultado), .btnL(btnL), .btnC(btnC), .led(led[7:0]));
+// Multiplexor para cambiar entre salida de resultado e indicadores
+assign uo = (ui_7 == 0) ? resultado : {4'b0, Carry, Overflow, Negative, Zero};
 
 endmodule
+
