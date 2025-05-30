2ef28906e5bd13d8dd581e92085c2641369ea830
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 20:22:07 -0600

    Update SR1.v

--- src/SR1.v
+++ src/SR1.v
@@ -1,9 +1,20 @@
 `timescale 1ns / 1ps
 
-module SR1(input wire [7:0] Desplazar,  // Entrada de 8 bits
-           input wire [4:0] Cantidad,   // Señal de control para el desplazamiento
-           output wire [7:0] Desplazados);  // Salida de 8 bits desplazados
+module SR1(
+    Desplazar,
+    Cantidad,
+    Desplazados
+);
 
-assign Desplazados = Desplazar >> Cantidad;  // Desplazamiento lógico a la derecha
+    input [7:0] Desplazar;         // Entrada de 8 bits
+    input [2:0] Cantidad;          // Cantidad de desplazamiento (0 a 7)
+    output [7:0] Desplazados;      // Salida de 8 bits desplazados
+
+    wire [7:0] Desplazar;
+    wire [2:0] Cantidad;
+    wire [7:0] Desplazados;
+
+    assign Desplazados = Desplazar >> Cantidad;   // Desplazamiento lógico a la derecha
 
 endmodule
+
