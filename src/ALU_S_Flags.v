8b77ee9d56335768ce4a260d6841593e7cf3ce91
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 21:01:52 -0600

    Update ALU_S_Flags.v

--- src/ALU_S_Flags.v
+++ src/ALU_S_Flags.v
@@ -1,17 +1,40 @@
-`timescale 1ns / 1ps
+module ALU_S_Flags(
+    input wire [7:0] A, B,
+    input wire [2:0] ALUControl,
+    input wire [2:0] Cantidad,  // Ajustado a 3 bits como el módulo ALU_S
+    output wire [7:0] Resultado,
+    output wire Carry,
+    output wire Overflow,
+    output wire Negative,
+    output wire Zero
+);
 
-module ALU_S_Flags(input wire [7:0] A, B, 
-                   input wire [2:0] ALUControl,
-                   input wire [4:0] Cantidad,
-                   output wire [7:0] Resultado,
-                   output wire Carry, Overflow, Negative, Zero);
+    wire [7:0] S_M;
+    wire C_out;
 
-wire S_M, C_out;
+    // Para ALU_S
+    ALU_S ALU_1 (
+        .A(A),
+        .B(B),
+        .ALUControl(ALUControl),
+        .Cantidad(Cantidad),
+        .S_M(S_M),
+        .C_out(C_out),
+        .Resultado(Resultado)
+    );
 
-// Para ALU_S
-ALU_S ALU_1 (.B(B), .A(A), .Cantidad(Cantidad), .ALUControl(ALUControl), .S_M(S_M), .C_out(C_out), .Resultado(Resultado));
-
-// Para Indicadores
-Indicadores Indicadores1 (.B(B), .A(A), .ALUControl(ALUControl), .Suma(S_M), .C_out(C_out), .Resultado(Resultado), .Carry(Carry), .Overflow(Overflow), .Negative(Negative), .Zero(Zero));
+    // Para Indicadores
+    Indicadores Indicadores1 (
+        .A(A),
+        .B(B),
+        .ALUControl(ALUControl),
+        .Suma(S_M),
+        .C_out(C_out),
+        .Resultado(Resultado),
+        .Carry(Carry),
+        .Overflow(Overflow),
+        .Negative(Negative),
+        .Zero(Zero)
+    );
 
 endmodule
