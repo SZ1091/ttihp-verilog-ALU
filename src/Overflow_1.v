ea7909be4657b6b802e29745752f08b52e1ee732
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 20:52:52 -0600

    Update Overflow_1.v

--- src/Overflow_1.v
+++ src/Overflow_1.v
@@ -1,8 +1,7 @@
-`timescale 1ns / 1ps
-
-module Overflow_1(input wire [7:0] A, B, Resultado,
-                  output wire Overflow);
-
-assign Overflow = (A[7] == B[7]) && (A[7] != Resultado[7]); 
-
+module Overflow_1(
+    input wire [7:0] A, B, Resultado,
+    output wire Overflow
+);
+    assign Overflow = (A[7] == B[7]) && (A[7] != Resultado[7]);
 endmodule
+
