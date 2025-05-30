bc9d170a62bac3439e50250a0d0b1ee8c333ab2e
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 20:39:55 -0600

    Update Sumador_0.v

--- src/Sumador_0.v
+++ src/Sumador_0.v
@@ -1,19 +1,13 @@
-`timescale 1ns / 1ps
-
-module Sumador_0(A, B, C_in, S, C_out);
-
-    input A, B, C_in;
-    output S, C_out;
-
-    wire A, B, C_in;
-    wire S, C_out;
-
-    wire p, g; // nodos internos
-
+module Sumador_0(
+    input wire A,
+    input wire B,
+    input wire C_in,
+    output wire S,
+    output wire C_out
+);
+    wire p, g; //nodos internos
     assign p = A ^ B;
     assign g = A & B;
     assign S = p ^ C_in;
     assign C_out = g | (p & C_in);
-
 endmodule
-
