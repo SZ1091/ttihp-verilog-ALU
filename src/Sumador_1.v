1dd64e4c4c74bb8f783caeab3b6aa64844aadaf7
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 20:05:47 -0600

    Update Sumador_1.v

--- src/Sumador_1.v
+++ src/Sumador_1.v
@@ -1,13 +1,34 @@
 `timescale 1ns / 1ps
 
-module Sumador_1(input wire [1:0] A, B, // 2 bits de entrada
-                 input wire C_in,
-                 output wire [1:0] S,
-                 output wire C_out);
+module Sumador_1(A, B, C_in, S, C_out);
 
-wire c_internal;
+    input [1:0] A, B;       // 2 bits de entrada
+    input C_in;
+    output [1:0] S;
+    output C_out;
 
-Sumador_0 Sumador01 (.A(A[0]), .B(B[0]), .C_in(C_in), .S(S[0]), .C_out(c_internal)); // lo que se envía al primer sumador
-Sumador_0 Sumador02 (.A(A[1]), .B(B[1]), .C_in(c_internal), .S(S[1]), .C_out(C_out));
+    wire [1:0] A, B;
+    wire C_in;
+    wire [1:0] S;
+    wire C_out;
+
+    wire c_internal;
+
+    Sumador_0 Sumador01 (
+        .A(A[0]),
+        .B(B[0]),
+        .C_in(C_in),
+        .S(S[0]),
+        .C_out(c_internal)
+    );
+
+    Sumador_0 Sumador02 (
+        .A(A[1]),
+        .B(B[1]),
+        .C_in(c_internal),
+        .S(S[1]),
+        .C_out(C_out)
+    );
 
 endmodule
+
