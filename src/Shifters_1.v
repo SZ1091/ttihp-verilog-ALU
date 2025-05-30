37aa3153698d67a4c3e362a3bddf7278bd5c8906
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 21:12:36 -0600

    Update Shifters_1.v

--- src/Shifters_1.v
+++ src/Shifters_1.v
@@ -1,6 +1,6 @@
 module Shifters_1(
     input wire [7:0] A, B,
-    input wire [2:0] Cantidad,
+    input wire [1:0] Cantidad,
     output wire [7:0] SL_A, SR_A, SL_B, SR_B
 );
     SL1 SLA (.Desplazar(A), .Cantidad(Cantidad), .Desplazados(SL_A));
