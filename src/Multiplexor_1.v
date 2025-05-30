4a339a1c485e13b4033b2addb462e81c296b6716
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 20:42:46 -0600

    Update Multiplexor_1.v

--- src/Multiplexor_1.v
+++ src/Multiplexor_1.v
@@ -1,18 +1,10 @@
-`timescale 1ns / 1ps
-
-module Multiplexor_1(B, ALUControl_0, B_S);
-
-    input [7:0] B;              // Entrada de 8 bits
-    input ALUControl_0;         // Señal de control
-    output [7:0] B_S;           // Salida de 8 bits
-
-    wire [7:0] B;
-    wire ALUControl_0;
-    wire [7:0] B_S;
-
+module Multiplexor_1(
+    input wire [7:0] B,
+    input wire ALUControl_0,
+    output wire [7:0] B_S
+);
     wire [7:0] z;
-
-    assign z = ~B;                      // Niega cada bit de B
-    assign B_S = ALUControl_0 ? z : B;  // Mux controlado por ALUControl_0
-
+    assign z = ~B;
+    assign B_S = ALUControl_0 ? z : B;
 endmodule
+
