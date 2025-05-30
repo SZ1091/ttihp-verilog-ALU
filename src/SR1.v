0e0541074313b45fe8bd2db8b880d5590d9a7f21
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 21:12:22 -0600

    Update SR1.v

--- src/SR1.v
+++ src/SR1.v
@@ -1,6 +1,6 @@
 module SR1(
     input wire [7:0] Desplazar,
-    input wire [2:0] Cantidad,
+    input wire [1:0] Cantidad,
     output wire [7:0] Desplazados
 );
     assign Desplazados = Desplazar >> Cantidad;
