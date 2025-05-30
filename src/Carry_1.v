fa2ee4d4862b5167716f9bbaecc659d449f4a97f
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 20:55:13 -0600

    Update Carry_1.v

--- src/Carry_1.v
+++ src/Carry_1.v
@@ -1,10 +1,10 @@
-`timescale 1ns / 1ps
-
-module Carry_1(input wire C_out, ALUControl_1,
-               output wire Carry);
-
-wire a;
-assign a = ~ALUControl_1;   // Aplica NOT a ALUControl_1
-assign Carry = a & C_out;   // Calcula la salida con la compuerta AND
-
+module Carry_1(
+    input wire C_out,
+    input wire ALUControl_1,
+    output wire Carry
+);
+    wire a;
+    assign a = ~ALUControl_1;
+    assign Carry = a & C_out;
 endmodule
+
