32e5860244a271f0e9452f47ff3c6a80a25adf74
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 20:57:09 -0600

    Update Negative_1.v

--- src/Negative_1.v
+++ src/Negative_1.v
@@ -1,8 +1,6 @@
-`timescale 1ns / 1ps
-
-module Negative_1(input wire [7:0] Resultado,
-                  output wire Negative);
-
-assign Negative = Resultado[7]; // Extrae el bit más significativo para indicar si el número es negativo
-
+module Negative_1(
+    input wire [7:0] Resultado,
+    output wire Negative
+);
+    assign Negative = Resultado[7];
 endmodule
