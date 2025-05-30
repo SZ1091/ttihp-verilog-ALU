81427bc5d65b0dbc7a42ad35a12461e6da449069
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 20:59:59 -0600

    Update Indicadores.v

--- src/Indicadores.v
+++ src/Indicadores.v
@@ -1,20 +1,38 @@
-`timescale 1ns / 1ps
+module Indicadores(
+    input wire [2:0] ALUControl,
+    input wire [7:0] Suma, A, B, Resultado,
+    input wire C_out,
+    output wire Carry,
+    output wire Overflow,
+    output wire Negative,
+    output wire Zero
+);
 
-module Indicadores(input wire [2:0] ALUControl,
-                   input wire [7:0] Suma, A, B, Resultado,
-                   input wire C_out,
-                   output wire Carry, Overflow, Negative, Zero);
+    // Para Zero
+    Zero_1 Zero1 (
+        .Resultado(Resultado),
+        .Zero(Zero)
+    );
 
-// Para Zero
-Zero_1 Zero1 (.Resultado(Resultado), .Zero(Zero));
+    // Para Negative
+    Negative_1 Negative1 (
+        .Resultado(Resultado),
+        .Negative(Negative)
+    );
 
-// Para Negative
-Negative_1 Negative1 (.Resultado(Resultado), .Negative(Negative));
+    // Para Carry
+    Carry_1 Carry1 (
+        .C_out(C_out),
+        .ALUControl_1(ALUControl[1]),
+        .Carry(Carry)
+    );
 
-// Para Carry
-Carry_1 Carry1 (.C_out(C_out), .ALUControl_1(ALUControl[1]), .Carry(Carry));
-
-// Para Overflow
-Overflow_1 Overflow1 (.A(A), .B(B), .Resultado(Resultado), .Overflow(Overflow));
+    // Para Overflow
+    Overflow_1 Overflow1 (
+        .A(A),
+        .B(B),
+        .Resultado(Resultado),
+        .Overflow(Overflow)
+    );
 
 endmodule
