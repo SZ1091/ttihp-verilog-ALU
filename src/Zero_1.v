3fc397f0fa8df46bc37cef7518cc624f50b8c236
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-29 20:58:17 -0600

    Update Zero_1.v

--- src/Zero_1.v
+++ src/Zero_1.v
@@ -1,8 +1,7 @@
-`timescale 1ns / 1ps
-
-module Zero_1(input wire [7:0] Resultado,
-              output wire Zero);
-
-assign Zero = (Resultado == 8'b0);   // Compara si el resultado es cero
-
+module Zero_1(
+    input wire [7:0] Resultado,
+    output wire Zero
+);
+    assign Zero = (Resultado == 8'b00000000);
 endmodule
+
